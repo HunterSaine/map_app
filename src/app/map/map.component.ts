bae5481abe96a1b162387d8e4553de51558a25b2
HunterSaine <112508289+HunterSaine@users.noreply.github.com>
2024-04-01 19:37:17 -0400

    displays values

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -7,12 +7,17 @@ import { ApiService } from '../api.service';
   styleUrl: './map.component.css'
 })
 export class MapComponent {
-
+  country: any = {}
   constructor (private apiService: ApiService) {}
 
   setCountryData(event: any) {
     console.log("event", event.target.id);
-    this.apiService.setCountryData(event.target.id).subscribe(data => console.log('DATA!!!', data))
+    this.apiService.setCountryData(event.target.id).subscribe((data: any) => {
+      console.log(data)
+      this.country = {
+        ...data,
+      }
+    })
     
   }
   
