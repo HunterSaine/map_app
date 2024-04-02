2f517227d9b410ed23fec4b87a471d1464359975
huntersaine <hunter.saine@gmail.com>
2024-04-01 21:09:29 -0400

    added css styling

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,32 +1,29 @@
-import { HttpClient} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
-  
   fetchCountryData(country: string) {
-    let subject = new Subject
+    let subject = new Subject();
     let api = `https://api.worldbank.org/v2/country/${country}?format=json`;
-    
-    
-    
+
     return this.http.get(api);
   }
-  
+
   setCountryData(country: string) {
     let subject = new Subject();
     this.fetchCountryData(country).subscribe((data: any) => {
       subject.next({
-        country: data[1][0].name ,
-        capital: data[1][0].capitalCity ,
-        region: data[1][0].region.value
-      })
-    })
-    return subject.asObservable()
+        country: data[1][0].name,
+        capital: data[1][0].capitalCity,
+        region: data[1][0].region.value,
+      });
+    });
+    return subject.asObservable();
   }
 }
